0d908fcd468b70d5dc6a16c13f1cb5e6e785abb0
AkhtarZaman12 <akhtar.tarar12@gmail.com>
2022-04-09 21:18:00 -0500

    localhost

--- index.js
+++ index.js
@@ -8,29 +8,31 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 import teamController from './src/controllers/team-controller.js';
 import AnnouncementController from './src/controllers/announcement.js';
 import ScheduleGamesController from './src/controllers/scheduled-games.js';
-import sgMail from '@sendgrid/mail'
-import cors from 'cors'
+import sgMail from '@sendgrid/mail';
+import cors from 'cors';
 
-sgMail.setApiKey('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')
+sgMail.setApiKey(
+  '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'
+);
 
 dotenv.config({ path: './config.env' });
 const app = express();
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-}))
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
+);
 app.use(express.json());
 app.disable('x-powered-by');
 
 app.post('/login', userController.login);
 app.post('/register-user', userController.register);
-app.post('/update-user',authenticate, userController.updateUser);
+app.post('/update-user', authenticate, userController.updateUser);
 app.post('/invite-user', authenticate, userController.invite);
 app.post('/team', authenticate, teamController.getTeam);
 app.post('/team-info', authenticate, teamController.getTeamInfo);
 app.post('/update-team-info', authenticate, teamController.updateTeamInfo);
-
 
 app.post('/request-reset-password', userController.requestResetPassword);
 app.post('/reset-password', userController.resetPassword);
@@ -63,8 +65,16 @@ app.post('/schedule-game', authenticate, ScheduleGamesController.scheduleGame);
 app.post('/scheduled-game', authenticate, ScheduleGamesController.getGames);
 app.post('/edit-game', authenticate, ScheduleGamesController.editGame);
 
-app.post('/update-game-status', authenticate, ScheduleGamesController.UpdateGameStatus);
-app.post('/update-game-users', authenticate, ScheduleGamesController.updateGameUsers);
+app.post(
+  '/update-game-status',
+  authenticate,
+  ScheduleGamesController.UpdateGameStatus
+);
+app.post(
+  '/update-game-users',
+  authenticate,
+  ScheduleGamesController.updateGameUsers
+);
 
 app.post('/invite-users', authenticate, userController.inviteUsers);
 app.post('/user-invitation', userController.userInvitation);
@@ -72,7 +82,9 @@ app.post('/delete-user', authenticate, userController.deleteUser);
 
 const PORT = process.env.PORT || 4000;
 (async () => {
-  const url = 'mongodb+srv://octane:EYwngdDZ7QtvFB8@octane.mhi3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/octane';
+  const url =
+    'mongodb+srv://octane:EYwngdDZ7QtvFB8@octane.mhi3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ||
+    'mongodb://127.0.0.1:27017/octane';
   // const url = 'mongodb://127.0.0.1:27017/octane';
 
   try {
