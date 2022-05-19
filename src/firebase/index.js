6888e6f3ea6b2fa57a81c9a0b223c8592c343fc7
AkhtarZaman12 <akhtar.tarar12@gmail.com>
2022-05-19 00:04:25 -0500

    token exception

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,40 +1,39 @@
 import pkg from 'firebase-admin';
-import User from '../modals/user.js'
-const {messaging} = pkg;
+import User from '../modals/user.js';
+const { messaging } = pkg;
 
-const sendPushNotification = async ( body, teamId) => {
-    const users = await User.find({teamId: teamId});
-    let tokens = users.map(user => user.firebaseToken);
-    tokens=tokens.filter(token => token);
-    console.log('tokens', tokens)
-    const payload = {
-        notification: {
-        title: 'Octane',
-        body: body,
-        },
-    };
-    const options = {
-        priority: 'high',
-        timeToLive: 60 * 60 * 24,
-    };
-    return await messaging().sendToDevice(tokens, payload, options);
-}
-const sendPushChatNotification = async ( body, token) => {
+const sendPushNotification = async (body, teamId) => {
+  const users = await User.find({ teamId: teamId });
+  let tokens = users.map((user) => user.firebaseToken);
+  tokens = tokens.filter((token) => token);
+  if (tokens?.length < 1) {
+    return;
+  }
+  const payload = {
+    notification: {
+      title: 'Octane',
+      body: body,
+    },
+  };
+  const options = {
+    priority: 'high',
+    timeToLive: 60 * 60 * 24,
+  };
+  return await messaging().sendToDevice(tokens, payload, options);
+};
+const sendPushChatNotification = async (body, token) => {
+  if (!token || token?.length < 1) return;
+  const payload = {
+    notification: {
+      title: 'Octane',
+      body: body,
+    },
+  };
+  const options = {
+    priority: 'high',
+    timeToLive: 60 * 60 * 24,
+  };
+  return await messaging().sendToDevice(token, payload, options);
+};
 
-    const payload = {
-        notification: {
-        title: 'Octane',
-        body: body,
-        },
-    };
-    const options = {
-        priority: 'high',
-        timeToLive: 60 * 60 * 24,
-    };
-    return await messaging().sendToDevice(token, payload, options);
-}
-
-export{
-    sendPushNotification,
-    sendPushChatNotification
-}
+export { sendPushNotification, sendPushChatNotification };
