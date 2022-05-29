906da65e5011ef478d0ac9e71ea24b5075c1ac98
ahmed-shafique <ahmed.shafique94@gmail.com>
2022-05-29 12:46:23 +0500

    Password Reset Email Changes

--- src/utils/mailer/index.js
+++ src/utils/mailer/index.js
@@ -19,10 +19,101 @@ async function sendMail(to, subject) {
       from: "no-reply@octanetech.ca",
       //from: "azcodes12@gmail.com",
       to: [to],
-      subject: subject,
+      subject: "Password Reset",
       text: "Octane",
-      html: "<b>Octane Team?</b>",
+      html: `   <div style="border:30px solid #07243e; padding: 5%;">
+      <div
+        style="
+           text-align: center;
+          "
+      >
+        <img
+          style=" width: 200px; height: 200px"
+          src="https://firebasestorage.googleapis.com/v0/b/octane-a916b.appspot.com/o/octane.png?alt=media&token=f4dc8529-8992-4942-bffd-aa4f51108388"
+        />
+      </div>
+      <p
+        style="
+            font-size: 32px;
+            font-weight: bold;
+            text-align: center;
+            align-self: center;
+            width: 100%;
+          "
+      >
+        Use below code to Reset your Password!
+      </p>
+
+      <p
+        style="
+            font-size: 32px;
+            font-weight: bold;
+            text-align: center;
+            align-self: center;
+            width: 100%;
+          "
+      >
+        ${subject}
+      </p>
+     
+
+      <p>
+        Octane is a mobile + web app for teams to collaborate, communicate and
+        stay organized for all games and practices! Use the mobile app to chat
+        with your teammates, let your coach know if you are unable to make it
+        to upcoming events and manage your health reports to stay safe and
+        protected while having fun.
+        <strong>Join and spread the word today!</strong>
+      </p>
+
+      <p style="color: grey; text-align: center;">
+        Octane Tech - Toronto, ON, CANADA
+      </p>
+      <p style="color: grey; text-align: center;">
+        Octane Tech is private held by ALM Inc.| Privacy Policy | Terms &
+        Conditions
+      </p>
+
+      <p style="font-weight: bold; text-align: center;">
+        VISIT US ON YOUR FAVORITE SOCIAL MEDIA CHANNEL
+      </p>
+      <div
+        style="
+        text-align: center;
+          "
+      >
+        <a href="https://www.facebook.com/Octane-Tech-104035735576947/">
+          <img
+            style="height: 50px; width: 50px"
+            src="https://firebasestorage.googleapis.com/v0/b/octane-a916b.appspot.com/o/fb1.png?alt=media&token=fb669f8b-cf10-48a0-bb1b-2b2046994d17"
+            alt="facebook"
+          />
+        </a>
+        <a href="https://www.instagram.com/octane.technology/">
+          <img
+            style="height: 50px; width: 50px"
+            src="https://firebasestorage.googleapis.com/v0/b/octane-a916b.appspot.com/o/insta.png?alt=media&token=adbf8dd7-5853-457c-950d-82a0279eb96a"
+            alt="instagram"
+          />
+        </a>
+        <a href="https://www.tiktok.com/@octanetech">
+          <img
+            style="height: 50px; width: 50px"
+            src="https://firebasestorage.googleapis.com/v0/b/octane-a916b.appspot.com/o/tiktok.png?alt=media&token=261fbdeb-ef0c-4ed5-a1bc-df680ef939b5"
+            alt="tiktok"
+          />
+        </a>
+        <a href="https://twitter.com/OctaneTech1">
+          <img
+            style="height: 50px; width: 50px"
+            src="https://firebasestorage.googleapis.com/v0/b/octane-a916b.appspot.com/o/tw1.png?alt=media&token=fb2e57ea-119e-4e9b-bca7-20ec23931e56"
+            alt="twitter"
+          />
+        </a>
+      </div>
+    </div>`,
     });
+    console.log(info);
     if (info.messageId) {
       return true;
     }
@@ -50,7 +141,7 @@ const sendInvitation = async (email, teamName, code) => {
   try {
     let transporter = nodemailer.createTransport({
       host: "smtp.ionos.com",
-      //service: "office365",
+      //service: "Gmail",
 
       auth: {
         //  user: "ahmed.shafique94@gmail.com",
